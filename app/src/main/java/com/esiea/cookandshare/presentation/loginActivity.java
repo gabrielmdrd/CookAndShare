f611d383ef498ca8129af80556806f3222253ddc
s4ndykiller <abdelmohand@gmail.com>
2018-11-27 01:30:11 +0100

    Modification de: - Home activity (taille boutons, police, position, logo)
    Ajout de:
    - Login activity
    - Inscription acitivity
    - De l'anglais et du français

---
+++ app/src/main/java/com/esiea/cookandshare/presentation/loginActivity.java
@@ -0,0 +1,15 @@
+package com.esiea.cookandshare.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esiea.cookandshare.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
